30a0ede20fd2bab636ce14cf555da05169d3434a
michael <michael.jos.mccarthy@gmail.com>
2024-01-04 22:48:06 -0500

    Update output var names

--- terraform-aws-nat-instance/output.tf
+++ terraform-aws-nat-instance/output.tf
@@ -1,19 +1,19 @@
-output "aws_security_group" {
+output "security_group" {
   value       = aws_security_group.this
   description = "Security group of NAT instance"
 }
 
-output "aws_network_interface" {
+output "network_interface" {
   value       = aws_network_interface.this
   description = "Network interface of NAT instance"
 }
 
-output "aws_launch_template" {
+output "launch_template" {
   value       = aws_launch_template.this
   description = "Launch template of NAT instance"
 }
 
-output "aws_autoscaling_group" {
+output "autoscaling_group" {
   value       = aws_autoscaling_group.this
   description = "Autoscaling group of NAT instance"
 }
